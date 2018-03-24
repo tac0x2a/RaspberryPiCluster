093e15762d3af9fe0e774313967cfb585b58bf48
TAC <tac@tac42.net>
2018-03-24 11:48:45 +0900

    NOT WORK add spark-defaults.conf

--- roles/hadoop/templates/etc/profile.d/hadoop.sh
+++ roles/hadoop/templates/etc/profile.d/hadoop.sh
@@ -1,3 +1,6 @@
+
+export HADOOP_CONF_DIR=/usr/src/hadoop-3.0.0/etc/hadoop/
+export YARN_CONF_DIR=/usr/src/hadoop-3.0.0/etc/hadoop/
 export HADOOP_USER_NAME=hadoop
 export HADOOP_HOME=/usr/src/hadoop-3.0.0
 export PATH=$PATH:$HADOOP_HOME/bin
